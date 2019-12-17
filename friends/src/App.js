619ed17beae965b8a6483a1b66e7b9dbca76514d
thesoundfromthesky <nagara@naver.com>
2019-12-17 11:59:59 -0600

    spinner for login

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import "./App.css";
 import { Route } from "react-router-dom";
+
+import "./App.css";
 import Login from "./Login";
 import Navigation from "./Navigation";
 
 // ## Build the App!
 // * Add a route for a login page and build out a simple login form
-
 // * The login function should save the returned token to localStorage.
 // You can setup `isLoading` state in your Login component,
 // and show a spinner on your form or in your button while the login request is happening.
+
 // * When the request returns, save the token to `localStorage`,
 // then use the history object in your Login component to navigate your user to your FriendsList route
 // * Create a `<PrivateRoute />` component to protect your other routes.
--- friends/src/Login.js
+++ friends/src/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-
+import axios from "axios";
+import Spinner from "./Spinner";
 // with username and password inputs and a submit button (design this however you would like).
+// * The login function should save the returned token to localStorage.
+// You can setup `isLoading` state in your Login component,
+// and show a spinner on your form or in your button while the login request is happening.
+
 export default function Login() {
+  function login(user, resetForm) {
+    axios.post("http://localhost:5000/api/login", user).then(data => {
+      console.log(data);
+      resetForm();
+    });
+  }
+
   return (
     <div>
       <Formik
@@ -16,11 +28,12 @@ export default function Login() {
             .max(20, "Must be 20 characters or less")
             .required("Required")
         })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
+        onSubmit={(values, { resetForm }) => {
+          console.log(values);
+          login(
+            { username: "Lambda School", password: "i<3Lambd4" },
+            resetForm
+          );
         }}
       >
         {({ isSubmitting }) => (
@@ -39,11 +52,16 @@ export default function Login() {
               type="password"
               name="password"
               placeholder="type password"
+              autoComplete="off"
             />
             <ErrorMessage name="password" component="div" />
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
+            {isSubmitting ? (
+              <Spinner />
+            ) : (
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            )}
           </Form>
         )}
       </Formik>
