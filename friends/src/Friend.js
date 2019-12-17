b1060e36fa5ebf321d1e7bf0831cb88d03705bfb
thesoundfromthesky <nagara@naver.com>
2019-12-17 12:53:26 -0600

    get friendslist

---
+++ friends/src/Friend.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// {
+//     id: 1
+//     name: 'Joe',
+//     age: 24,
+//     email: 'joe@lambdaschool.com',
+// }
+export default function Friend({ friend }) {
+  const { name, age, email } = friend;
+  return (
+    <div>
+      <div>Name: {name}</div>
+      <div>Age: {age}</div>
+      <div>Email: {email}</div>
+    </div>
+  );
+}
--- friends/src/FriendsList.js
+++ friends/src/FriendsList.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Friend from "./Friend";
 
 export default function FriendsList() {
-    return (
-        <div>
-            adfsasfd
-        </div>
-    )
+  const [friendsList, setFriendsList] = useState([]);
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    axios
+      .create({
+        baseURL: "http://localhost:5000",
+        headers: {
+          Authorization: `${token}`
+        }
+      })
+      .get("/api/friends")
+      .then(({ data }) => {
+        setFriendsList(data);
+      });
+  }, []);
+  return (
+    <div>
+      {friendsList.map(friend => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </div>
+  );
 }
